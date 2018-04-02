72b6141c983239603171095655c361fb0ad732e7
weiqiangcai <cwq915954109@gmail.com>
2018-04-02 19:43:15 +0800

    增加一个测试

---
+++ ceshi/ceshi/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ceshi
+//
+//  Created by 蔡伟强 on 2018/4/2.
+//  Copyright © 2018年 蔡伟强. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ceshi/ceshi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ceshi
+//
+//  Created by 蔡伟强 on 2018/4/2.
+//  Copyright © 2018年 蔡伟强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
